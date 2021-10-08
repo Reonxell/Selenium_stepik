72f6934d512b6fed2c84e1064dc9ec3c0322c7c9
Reonxell <66777647+Reonxell@users.noreply.github.com>
2021-10-08 10:22:49 +0300

    Delete test_language directory

--- test_language/test_items.py
+++
@@ -1,8 +0,0 @@
-import time
-
-
-def test_items(app):
-    app.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    time.sleep(30)
-    assert app.find_element_by_css_selector(
-        '.btn-add-to-basket').is_displayed() == True, 'Кнопка добавления в корзину не кативна'
