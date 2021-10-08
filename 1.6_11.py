5ab31405c7b1775d3b77f752116353eee92e0d94
Reonxell <66777647+Reonxell@users.noreply.github.com>
2021-10-08 10:15:41 +0300

    Delete 1.6_11.py

--- 1.6_11.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.expected_conditions import text_to_be_present_in_element
-from selenium.webdriver.support.ui import WebDriverWait
-
-driver = webdriver.Chrome()
-WebDriverWait(driver, 5).until(text_to_be_present_in_element((By.ID, 'sadas'), 'dsadsaasdsad'))
